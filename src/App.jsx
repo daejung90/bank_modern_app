cce6e44f433566cbca2671c7e37c1e15dbe5a525
daejung90 <djung.ca@gmail.com>
2023-06-28 11:42:52 -0400

    setting the navbar and the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./style";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`styles.boxWidth`}>Navbar</div>
+      </div>
+      <div
+        className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}></div>
+      <div className={`${styles.boxWidth}`}>
+        Stats Business Billing CardDeal Testimonials Clients CTA Footer
+      </div>
+    </div>
+  );
 };
 
 export default App;
